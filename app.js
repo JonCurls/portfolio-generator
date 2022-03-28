60a7ab8896314e7089bec1f28e10d9456de09a14
MrCurls <JonCurls@gmail.com>
2022-03-28 13:00:41 -0500

    filter and map

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const inquirer = require("inquirer");
-// const fs = require("fs");
-// const generatePage = require("./src/page-template");
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile("./index.html", pageHTML, (err) => {
-//   if (err) throw err;
-
-//   console.log("Portfolio complete! Check out index.html to see the output!");
-// });
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -150,5 +143,11 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw err;
+      console.log(
+        "Portfolio complete! Check out index.html to see the output!"
+      );
+    });
   });
